e11758814b1a0826457054ec984827d872235d90
dtyghc <100001963+dtyghc@users.noreply.github.com>
2022-07-28 18:53:36 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_mi439.mk
+    $(LOCAL_DIR)/dot_olives.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_mi439-user \
-    aosp_mi439-userdebug \
-    aosp_mi439-eng
+    dot_olives-user \
+    dot_olives-userdebug \
+    dot_olives-eng
